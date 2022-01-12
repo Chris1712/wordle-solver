8d0d213f3f86edf60cb5b0dcc2914930d66af50e
Chris <christurner5@gmail.com>
2022-01-12 01:02:23 +0000

    Add a dictionary service holding the raw data

--- dictionary-extraction/get-words.py
+++ dictionary-extraction/get-words.py
@@ -4,18 +4,31 @@ import re
 
 print('Finding and sorting all unique 5 letter words, without symbols...')
 
-valid_words = set()  # set to store our unqiue words
+valid_words_set = set()  # set to store our unqiue words
 
 # Read:
 with open("words.txt", "r") as source:
     for line in source:
         stripped = line.strip()
         if len(stripped) == 5 and re.match("^[A-z]{5}$", stripped):
-            valid_words.add(stripped.lower())
+          valid_words_set.add(stripped.lower())
 
-# Write:
+valid_words = sorted(valid_words_set)
+
+# Write plain:
 with open("5-letter-words.txt", "w") as dest:
     for word in sorted(valid_words):
-        dest.write(word + "\n")
+        dest.write(f'"{word}",\n')
+
+
+# Format with quotes:
+quoted_valid_words = map(lambda raw_word: f'"{raw_word}"', valid_words)
+
+# Join with commas and newlines:
+js_formatted_valid_words = ',\n'.join(quoted_valid_words)
+
+# Write formatted:
+with open("5-letter-words-formatted.txt", "w") as dest:
+    dest.write(js_formatted_valid_words)
 
 
--- src/app/word.service.ts
+++ src/app/word.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import {DictionaryService} from "./dictionary.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordService {
 
-  constructor() { }
+  constructor(private dictionaryService: DictionaryService) { }
+
+  // Determine letter preference (IE, which letters occur in most unique words?)
+
+  // First time, suggest word with most frequent letters
+  // Not optimal, doesn't consider optimal positions!
+  // Take input like 'wwwyg' and previous word
+  // Come up with a new word
 
   getWords(): string {
-    return "hi from wordservice!"
+    return this.dictionaryService.getDictionary()[21950]
   }
+
+
 }
